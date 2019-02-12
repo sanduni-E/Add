57205d397c42d66c206b79c55953c41bf92fe986
sanduni-E <sanduniue@gmail.com>
2019-02-12 20:18:46 +0530

    Add
    Sum of two numbers

---
+++ JavaApplication6.java
@@ -0,0 +1,12 @@
+
+public class JavaApplication6 {
+
+    
+    public static void main(String[] args) {
+        int x=10;
+        int y=30;
+int total=x+y;
+System.out.println("x+y="+(x+y));
+    }
+    
+}
